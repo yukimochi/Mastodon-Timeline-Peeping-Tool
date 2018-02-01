6119f97a8bb655681e2b3353e1a7fdfc47d64b88
Naoki Kosaka <n.k@mail.yukimochi.net>
2018-02-02 01:34:26 +0900

    Fixed by ESLint.

--- Timeline Peeping Tool/Scripts/index.js
+++ Timeline Peeping Tool/Scripts/index.js
@@ -2,11 +2,11 @@
 //This software is released under the MIT License.
 //Copyright (c) 2017 YUKIMOCHI Laboratory
 
-ws_connection = null
-last_request = null
-instance = null
-Blocking = false
-federate = null
+ws_connection = null;
+last_request = null;
+instance = null;
+Blocking = false;
+federate = null;
 
 window.onload = function () {
     var search_box = document.getElementsByClassName("search__input")[0];
@@ -19,53 +19,53 @@ window.onload = function () {
             refresh();
             get_instance(instance, insert_instance);
         }
-    })
+    });
     var ws_ltl = document.getElementsByClassName("start-ws-ltl")[0];
     var ws_ftl = document.getElementsByClassName("start-ws-ftl")[0];
     ws_ltl.addEventListener('click', function (event) {
         event.preventDefault();
-        if (Blocking != true) {
-            Blocking = true
-            federate = false
+        if (Blocking !== true) {
+            Blocking = true;
+            federate = false;
             ws_ltl.setAttribute('style', 'background-color:coral');
             ws_ftl.setAttribute('style', 'background-color:lightblue');
-            instance = search_box['value']
-            open_ws(instance, federate)
-            get_statases(instance, last_request, !federate, insert_toot)
+            instance = search_box['value'];
+            open_ws(instance, federate);
+            get_statases(instance, last_request, !federate, insert_toot);
             ws_connection.addEventListener('message', function (event) {
-                document.getElementsByClassName("poll-way")[0].textContent = 'Connect with WebSocket'
+                document.getElementsByClassName("poll-way")[0].textContent = 'Connect with WebSocket';
                 insert_toot(JSON.parse(event.data).payload, true, null);
             });
             Interval(3, get_statases, federate);
         }
-    })
+    });
     ws_ftl.addEventListener('click', function (event) {
         event.preventDefault();
-        if (Blocking != true) {
-            Blocking = true
-            federate = true
+        if (Blocking !== true) {
+            Blocking = true;
+            federate = true;
             ws_ltl.setAttribute('style', 'background-color:lightblue');
             ws_ftl.setAttribute('style', 'background-color:coral');
             instance = search_box['value'];
-            open_ws(instance, federate)
-            get_statases(instance, last_request, !federate, insert_toot)
+            open_ws(instance, federate);
+            get_statases(instance, last_request, !federate, insert_toot);
             ws_connection.addEventListener('message', function (event) {
-                document.getElementsByClassName("poll-way")[0].textContent = 'Connect with WebSocket'
+                document.getElementsByClassName("poll-way")[0].textContent = 'Connect with WebSocket';
                 insert_toot(JSON.parse(event.data).payload, true, null);
             });
             Interval(3, get_statases, federate);
         }
-    })
-}
+    });
+};
 
 function open_ws(instance, federate) {
     url = 'wss://' + instance + '/api/v1/streaming';
     if (federate) {
-        url += '?stream=public'
+        url += '?stream=public';
     } else {
-        url += '?stream=public:local'
+        url += '?stream=public:local';
     }
-    ws_connection = new WebSocket(url)
+    ws_connection = new WebSocket(url);
 }
 
 function refresh() {
@@ -73,17 +73,17 @@ function refresh() {
 }
 
 function Interval(sec, callback, ltl) {
-    var clock = 0
+    var clock = 0;
     var id = setInterval(function () {
         clock++;
         if (clock > sec) {
             clock = 0;
-            if (ws_connection.readyState != 1) {
-                document.getElementsByClassName("poll-way")[0].textContent = 'Connect with Polling'
+            if (ws_connection.readyState !== 1) {
+                document.getElementsByClassName("poll-way")[0].textContent = 'Connect with Polling';
                 callback(instance, last_request, !federate, insert_toot);
             }
         }
-    }, 1000)
+    }, 1000);
 }
 
 function insert_instance(text, err) {
@@ -95,23 +95,23 @@ function insert_instance(text, err) {
     document.getElementsByClassName("user-counter")[0].textContent = instance['stats']['user_count'];
     document.getElementsByClassName("toot-counter")[0].textContent = instance['stats']['status_count'];
     document.getElementsByClassName("connection-counter")[0].innerHTML = instance['stats']['domain_count'];
-    if (instance['thumbnail'] != null) {
+    if (instance['thumbnail'] !== null) {
         thumb = document.getElementsByClassName("instance_thumbnail")[0];
         thumb.setAttribute('style', 'background-image: url(' + instance['thumbnail'] + ')');
     }
 }
 
 function insert_toot(text, ws, err) {
-    entrys = processing_entrys(text, ws)
+    entrys = processing_entrys(text, ws);
 
-    if (entrys != null) {
+    if (entrys !== null) {
         entrys.forEach(entry => {
-            parent = document.getElementsByClassName("activity-stream")[0]
+            parent = document.getElementsByClassName("activity-stream")[0];
             child = parent.firstElementChild;
             parent.insertBefore(entry, child);
         });
     }
-    lock = false
+    lock = false;
 }
 
 function get_instance(instance, callback) {
@@ -122,22 +122,22 @@ function get_instance(instance, callback) {
         copy = xhr.responseText;
         xhr.abort();
         callback(copy, null);
-    }
+    };
     xhr.onerror = function (err) {
         callback(None, err);
-    }
-    xhr.send()
+    };
+    xhr.send();
 }
 
 function get_statases(instance, since_id, local, callback) {
     url = 'https://' + instance + '/api/v1/timelines/public';
     if (local) {
-        url += '?local=true'
-        if (since_id != null) {
+        url += '?local=true';
+        if (since_id !== null) {
             url += '&since_id=' + since_id;
         }
     } else {
-        if (since_id != null) {
+        if (since_id !== null) {
             url += '?since_id=' + since_id;
         }
     }
@@ -145,11 +145,11 @@ function get_statases(instance, since_id, local, callback) {
     xhr.open('GET', url);
     xhr.onload = function () {
         callback(xhr.responseText, false, null);
-    }
+    };
     xhr.onerror = function (err) {
         callback(None, err);
-    }
-    xhr.send()
+    };
+    xhr.send();
 }
 
 function processing_entrys(data, ws) {
@@ -161,7 +161,7 @@ function processing_entrys(data, ws) {
     if (statuses.length > 0) {
         last_request = statuses[0]['id'];
 
-        entrys = []
+        entrys = [];
         for (let idx = 0; idx < statuses.length; idx++) {
             const status = statuses[idx];
             try {
@@ -170,12 +170,12 @@ function processing_entrys(data, ws) {
                 status__header = html_status__header(status['url'], status['created_at'], account['url'], account['avatar'], account['display_name'], account['acct']);
                 status__content = html_status__content(status['content']);
 
-                md_att = []
+                md_att = [];
                 for (let idx_md_att = 0; idx_md_att < media_attachments.length; idx_md_att++) {
                     const media_attachment = media_attachments[idx_md_att];
                     md_att.push(media_attachment['url']);
                 }
-                MediaGallery = null
+                MediaGallery = null;
                 if (media_attachments.length > 0) {
                     MediaGallery = html_MediaGallery(md_att);
                 }
@@ -198,12 +198,12 @@ function html_entry(status__header, status__content, MediaGallery) {
     status.setAttribute('class', 'status light');
     status.appendChild(status__header);
     status.appendChild(status__content);
-    if (MediaGallery != null) {
+    if (MediaGallery !== null) {
         status.appendChild(MediaGallery);
     }
-    entry.appendChild(status)
+    entry.appendChild(status);
 
-    return entry
+    return entry;
 }
 
 function html_status__header(status_url, status_time, author_url, author_avatar_url, author_name, author_id) {
@@ -263,12 +263,12 @@ function html_status__content(text) {
     status__content.appendChild(e_content);
 
     return status__content;
-};
+}
 
 function html_MediaGallery(img_urls) {
     var media_gallery = document.createElement('div');
-    media_gallery.setAttribute('class', 'media-gallery')
-    media_gallery.setAttribute('style', 'height: 240px;')
+    media_gallery.setAttribute('class', 'media-gallery');
+    media_gallery.setAttribute('style', 'height: 240px;');
 
     img_urls.forEach(img_url => {
         var media_gallery__item = document.createElement('div');
@@ -286,4 +286,4 @@ function html_MediaGallery(img_urls) {
     });
 
     return media_gallery;
-};
+}
