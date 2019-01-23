4b0a4833d379711f5c9187d351f61acd17ae5489
Naoki Kosaka <n.k@mail.yukimochi.net>
2019-01-23 22:35:24 +0900

    Fully refactoring and multiple images support.

--- scripts/index.js
+++ scripts/index.js
@@ -1,139 +1,125 @@
 //Mastodon Timeline Peeping Tool Made by YUKIMOCHI(@YUKIMOCHI@toot.yukimochi.jp)
 //This software is released under the MIT License.
-//Copyright (c) 2017 YUKIMOCHI Laboratory
+//Copyright (c) 2019 YUKIMOCHI Laboratory
 
 urlparam = new Object;
-decodeURIComponent(location.search.substring(1)).split("&").forEach(value =>{
+decodeURIComponent(location.search.substring(1)).split("&").forEach(value => {
     var keyvalue = value.split("=");
     urlparam[keyvalue[0]] = keyvalue[1];
 })
-ws_connection = null;
 last_request = null;
-instance = urlparam.instance? urlparam.instance : null;
-Blocking = false;
-federate = urlparam.federate && urlparam.instance? (urlparam.federate==="1"?true:false) : null;
 
 window.onload = function () {
-    var search_box = document.getElementsByClassName("search__input")[0];
-    if(instance){
-        search_box.value = instance;
-        document.getElementsByClassName("search__input")[0].disabled=true;
-        show_instancepage();
+    var search_box = document.getElementById("instance_input");
+    if (urlparam.instance) {
+        search_box.value = urlparam.instance;
+        get_instance(insert_instance);
+        if (urlparam.federate && (urlparam.federate === "1" || urlparam.federate === "true")) {
+            show_timeline(true);
+        }
     }
     search_box.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
             event.preventDefault();
-            show_instancepage();
+            get_instance(insert_instance);
         }
     });
-    var ws_ltl = document.getElementsByClassName("start-ws-ltl")[0];
-    var ws_ftl = document.getElementsByClassName("start-ws-ftl")[0];
-    ws_ltl.addEventListener('click', function (event) {
-        event.preventDefault();
-        federate = false;
-        show_timeline(false);
+    document.getElementById("start-ws-ltl").addEventListener('click', function (event) {
+        if (last_request == null) {
+            event.preventDefault();
+            federate = false;
+            show_timeline(federate);
+        }
     });
-    ws_ftl.addEventListener('click', function (event) {
-        event.preventDefault();
-        federate = true;
-        show_timeline(true);
+    document.getElementById("start-ws-ftl").addEventListener('click', function (event) {
+        if (last_request == null) {
+            event.preventDefault();
+            federate = true;
+            show_timeline(federate);
+        }
     });
-    if(federate !== null){
-        show_timeline(federate);
-    }
 };
 
-function show_instancepage(){
-    var search_box = document.getElementsByClassName("search__input")[0];
-    var actions = document.getElementsByClassName("actions")[0];
-    instance = search_box['value'];
-    actions.setAttribute('style', '');
-    refresh();
-    get_instance(instance, insert_instance);
-}
-
-function show_timeline(bool_ftl){
-    if (Blocking !== true) {
-        Blocking = true;
-        var search_box = document.getElementsByClassName("search__input")[0];
-        var ws_ltl = document.getElementsByClassName("start-ws-ltl")[0];
-        var ws_ftl = document.getElementsByClassName("start-ws-ftl")[0];
-        if(bool_ftl){
-            ws_ltl.setAttribute('style', 'background-color:lightblue');
-            ws_ftl.setAttribute('style', 'background-color:coral');
-        }else{
-            ws_ltl.setAttribute('style', 'background-color:coral');
-            ws_ftl.setAttribute('style', 'background-color:lightblue');
-        }
-        instance = search_box['value'];
-        open_ws(instance, federate);
-        get_statases(instance, last_request, !federate, insert_toot);
-        ws_connection.addEventListener('message', function (event) {
-            document.getElementsByClassName("poll-way")[0].textContent = 'Connect with WebSocket';
-            insert_toot(JSON.parse(event.data).payload, true, null);
-        });
-        Interval(3, get_statases, federate);
+function show_timeline(federate) {
+    if (federate) {
+        document.getElementById("start-ws-ltl").setAttribute('style', 'background-color:lightblue');
+        document.getElementById("start-ws-ftl").setAttribute('style', 'background-color:coral');
+    } else {
+        document.getElementById("start-ws-ltl").setAttribute('style', 'background-color:coral');
+        document.getElementById("start-ws-ftl").setAttribute('style', 'background-color:lightblue');
     }
+
+    get_statases(last_request, federate, insert_toot);
+    open_connection(federate);
 }
 
-function open_ws(instance, federate) {
-    url = 'wss://' + instance + '/api/v1/streaming';
+function open_connection(federate) {
+    var instance = document.getElementById("instance_input")['value'];
+    var url = 'wss://' + instance + '/api/v1/streaming';
     if (federate) {
         url += '?stream=public';
     } else {
         url += '?stream=public:local';
     }
-    ws_connection = new WebSocket(url);
+    var ws_connection = new WebSocket(url);
+    ws_connection.addEventListener('open', function () {
+        document.getElementById("poll-way").textContent = 'Connect with WebSocket';
+    });
+    ws_connection.addEventListener('message', function (event) {
+        insert_toot(JSON.parse(event.data).payload, true, null);
+    });
+    ws_connection.addEventListener('close', function (event) {
+        document.getElementById("poll-way").textContent = 'Connect with XHR Polling';
+        setTimeout(function () {
+            xhr_loop();
+        }, 3000);
+    });
 }
 
-function refresh() {
-    last_request = null;
-}
-
-function Interval(sec, callback, ltl) {
-    var clock = 0;
-    var id = setInterval(function () {
-        clock++;
-        if (clock > sec) {
-            clock = 0;
-            if (ws_connection.readyState !== 1) {
-                document.getElementsByClassName("poll-way")[0].textContent = 'Connect with Polling';
-                callback(instance, last_request, !federate, insert_toot);
-            }
-        }
-    }, 1000);
+function xhr_loop() {
+    get_statases(last_request, federate, insert_toot);
+    setTimeout(function () {
+        xhr_loop();
+    }, 3000);
 }
 
 function insert_instance(text, err) {
-    instance = JSON.parse(text);
+    if (err) {
+        // TODO
+    } else {
+        document.getElementById("actions").setAttribute('style', '');
+        document.getElementById("instance_input").disabled = true;
+        instance = JSON.parse(text);
 
-    document.getElementsByClassName("instance_name")[0].textContent = instance['title'];
-    document.getElementsByClassName("instance_domain")[0].textContent = instance['uri'];
-    document.getElementsByClassName("instance_description")[0].innerHTML = instance['description'];
-    document.getElementsByClassName("user-counter")[0].textContent = instance['stats']['user_count'];
-    document.getElementsByClassName("toot-counter")[0].textContent = instance['stats']['status_count'];
-    document.getElementsByClassName("connection-counter")[0].innerHTML = instance['stats']['domain_count'];
-    if (instance['thumbnail'] !== null) {
-        thumb = document.getElementsByClassName("instance_thumbnail")[0];
-        thumb.setAttribute('style', 'background-image: url(' + instance['thumbnail'] + ')');
+        document.getElementById("instance_name").textContent = instance['title'];
+        document.getElementById("instance_domain").textContent = instance['uri'];
+        document.getElementById("instance_description").innerHTML = instance['description'];
+        document.getElementById("user-counter").textContent = instance['stats']['user_count'];
+        document.getElementById("toot-counter").textContent = instance['stats']['status_count'];
+        document.getElementById("connection-counter").innerHTML = instance['stats']['domain_count'];
+        if (instance['thumbnail'] !== null) {
+            thumb = document.getElementById("instance_thumbnail");
+            thumb.setAttribute('style', 'background-image: url(' + instance['thumbnail'] + ')');
+        }
     }
 }
 
 function insert_toot(text, ws, err) {
-    entrys = processing_entrys(text, ws);
+    var entrys = processing_entrys(text, ws);
 
     if (entrys !== null) {
         entrys.forEach(entry => {
-            parent = document.getElementsByClassName("activity-stream")[0];
-            child = parent.firstElementChild;
+            var parent = document.getElementsByClassName("activity-stream")[0];
+            var child = parent.firstElementChild;
             parent.insertBefore(entry, child);
         });
     }
-    lock = false;
 }
 
-function get_instance(instance, callback) {
-    url = 'https://' + instance + '/api/v1/instance';
+function get_instance(callback) {
+    var instance = document.getElementById("instance_input")['value'];
+    var url = 'https://' + instance + '/api/v1/instance';
+
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload = function () {
@@ -142,14 +128,16 @@ function get_instance(instance, callback) {
         callback(copy, null);
     };
     xhr.onerror = function (err) {
-        callback(None, err);
+        callback(null, err);
     };
     xhr.send();
 }
 
-function get_statases(instance, since_id, local, callback) {
-    url = 'https://' + instance + '/api/v1/timelines/public';
-    if (local) {
+function get_statases(since_id, federate, callback) {
+    var instance = document.getElementById("instance_input")['value'];
+    var url = 'https://' + instance + '/api/v1/timelines/public';
+
+    if (!federate) {
         url += '?local=true';
         if (since_id !== null) {
             url += '&since_id=' + since_id;
@@ -165,35 +153,35 @@ function get_statases(instance, since_id, local, callback) {
         callback(xhr.responseText, false, null);
     };
     xhr.onerror = function (err) {
-        callback(None, err);
+        callback(null, err);
     };
     xhr.send();
 }
 
 function processing_entrys(data, ws) {
     if (ws) {
-        statuses = [JSON.parse(data)];
+        var statuses = [JSON.parse(data)];
     } else {
-        statuses = JSON.parse(data);
+        var statuses = JSON.parse(data);
     }
     if (statuses.length > 0) {
         last_request = statuses[0]['id'];
 
-        entrys = [];
-        for (let idx = 0; idx < statuses.length; idx++) {
+        var entrys = [];
+        for (var idx = 0; idx < statuses.length; idx++) {
             const status = statuses[idx];
             try {
-                account = status['account'];
-                media_attachments = status['media_attachments'];
-                status__header = html_status__header(status['url'], status['created_at'], account['url'], account['avatar'], account['display_name'], account['acct']);
-                status__content = html_status__content(status['content']);
+                var account = status['account'];
+                var media_attachments = status['media_attachments'];
+                var status__header = html_status__header(status['url'], status['created_at'], account['url'], account['avatar'], account['display_name'], account['acct']);
+                var status__content = html_status__content(status['content']);
 
-                md_att = [];
-                for (let idx_md_att = 0; idx_md_att < media_attachments.length; idx_md_att++) {
+                var md_att = [];
+                for (var idx_md_att = 0; idx_md_att < media_attachments.length; idx_md_att++) {
                     const media_attachment = media_attachments[idx_md_att];
-                    md_att.push(media_attachment['url']);
+                    md_att.push(media_attachment['preview_url']);
                 }
-                MediaGallery = null;
+                var MediaGallery = null;
                 if (media_attachments.length > 0) {
                     MediaGallery = html_MediaGallery(md_att);
                 }
@@ -225,82 +213,62 @@ function html_entry(status__header, status__content, MediaGallery) {
 }
 
 function html_status__header(status_url, status_time, author_url, author_avatar_url, author_name, author_id) {
-    var status__header = document.createElement('div');
-    status__header.setAttribute('class', 'status__header');
+    var t = document.importNode(document.querySelector('#html_status__header').content, true);
+    t.querySelector('div.status__header div.status__meta a.status__relative-time.u-url.u-uid').setAttribute('href', status_url);
+    t.querySelector('div.status__header div.status__meta a.status__relative-time.u-url.u-uid time.time-ago').innerText = status_time;
+    t.querySelector('div.status__header a.status__display-name.p-author.h-card').setAttribute('href', author_url);
+    t.querySelector('div.status__header a.status__display-name.p-author.h-card div.status__avatar div img.u-photo').setAttribute('src', author_avatar_url);
+    t.querySelector('div.status__header a.status__display-name.p-author.h-card span.display-name strong.p-name.emojify').innerText = author_name;
+    t.querySelector('div.status__header a.status__display-name.p-author.h-card span.display-name span').innerText = "@" + author_id;
 
-    var status__meta = document.createElement('div');
-    status__meta.setAttribute('class', 'status__meta');
-    var status__relative_time = document.createElement('a');
-    status__relative_time.setAttribute('class', 'status__relative-time u-url u-uid');
-    status__relative_time.setAttribute('rel', 'noopener');
-    status__relative_time.setAttribute('href', status_url);
-    var time_ago = document.createElement('time');
-    time_ago.setAttribute('class', 'time-ago');
-    time_ago.textContent = status_time;
-    status__relative_time.appendChild(time_ago);
-    status__meta.appendChild(status__relative_time);
-
-    var status__display_name = document.createElement('a');
-    status__display_name.setAttribute('class', 'status__display-name p-author h-card');
-    status__display_name.setAttribute('rel', 'noopener');
-    status__display_name.setAttribute('href', author_url);
-    var status__avatar = document.createElement('div');
-    status__avatar.setAttribute('class', 'status__avatar');
-    var around_img = document.createElement('div');
-    var u_photo = document.createElement('img');
-    u_photo.setAttribute('class', 'u-photo');
-    u_photo.setAttribute('width', 48);
-    u_photo.setAttribute('height', 48);
-    u_photo.setAttribute('src', author_avatar_url);
-    around_img.appendChild(u_photo);
-    status__avatar.appendChild(around_img);
-    var display_name = document.createElement('span');
-    display_name.setAttribute('class', 'display-name');
-    var strong = document.createElement('strong');
-    strong.setAttribute('class', 'p-name emojify');
-    strong.textContent = author_name;
-    var span = document.createElement('span');
-    span.textContent = '@' + author_id;
-    display_name.appendChild(strong);
-    display_name.appendChild(span);
-    status__display_name.appendChild(status__avatar);
-    status__display_name.appendChild(display_name);
-
-    status__header.appendChild(status__meta);
-    status__header.appendChild(status__display_name);
-
-    return status__header;
+    return t;
 }
 
 function html_status__content(text) {
-    var status__content = document.createElement('div');
-    status__content.setAttribute('class', 'status__content p-name emojify');
-    var e_content = document.createElement('div');
-    e_content.setAttribute('class', 'e-content');
-    e_content.innerHTML = text;
-    status__content.appendChild(e_content);
+    var t = document.importNode(document.querySelector('#html_status__content').content, true);
+    t.querySelector('div.status__content.p-name.emojify div.e-content p').innerHTML = text;
 
-    return status__content;
+    return t;
 }
 
 function html_MediaGallery(img_urls) {
+    img_urls = img_urls.slice(0, 4);
     var media_gallery = document.createElement('div');
     media_gallery.setAttribute('class', 'media-gallery');
     media_gallery.setAttribute('style', 'height: 240px;');
 
+    var styles = "";
+    switch (img_urls.length) {
+        case 1:
+            styles = ["left: auto; top: auto; right: auto; bottom: auto; width: 100%; height: 100%;"];
+            break;
+        case 2:
+            styles = ["left: auto; top: auto; width: 50%; height: 100%; right: 2px; bottom: auto;",
+                "left: 2px; top: auto; width: 50%; height: 100%; right: auto; bottom: auto;"];
+            break;
+        case 3:
+            styles = ["left: auto; top: auto; width: 100%; height: 50%; right: 2px; bottom: 2px;",
+                "left: 2px; top: auto; width: 50%; height: 50%; right: auto; bottom: 2px;",
+                "left: auto; top: 2px; width: 50%; height: 50%; right: 2px; bottom: auto;"];
+            break;
+        case 4:
+            styles = ["left: auto; top: auto; width: 50%; height: 50%; right: 2px; bottom: 2px;",
+                "left: 2px; top: auto; width: 50%; height: 50%; right: auto; bottom: 2px;",
+                "left: auto; top: 2px; width: 50%; height: 50%; right: 2px; bottom: auto;",
+                "left: 2px; top: 2px; width: 50%; height: 50%; right: auto; bottom: auto;"];
+            break;
+        default:
+            break;
+    }
+
+    var num = 0;
     img_urls.forEach(img_url => {
-        var media_gallery__item = document.createElement('div');
-        media_gallery__item.setAttribute('class', 'media-gallery__item');
-        media_gallery__item.setAttribute('style', 'left: auto; top: auto; right: auto; bottom: auto; width: 100%; height: 100%;');
-        var media_gallery__item_thumbnail = document.createElement('a');
-        media_gallery__item_thumbnail.setAttribute('class', 'media-gallery__item-thumbnail');
-        media_gallery__item_thumbnail.setAttribute('href', img_url);
-        media_gallery__item_thumbnail.setAttribute('target', '_blank');
-        var img = document.createElement('img');
-        img.setAttribute('src', img_url);
-        media_gallery__item_thumbnail.appendChild(img);
-        media_gallery__item.appendChild(media_gallery__item_thumbnail);
-        media_gallery.appendChild(media_gallery__item);
+        var t = document.importNode(document.querySelector('#html_media__gallery_item').content, true);
+        t.querySelector('div.media-gallery__item').setAttribute('style', styles[num]);
+        t.querySelector('div.media-gallery__item a.media-gallery__item-thumbnail').setAttribute('href', img_url);
+        t.querySelector('div.media-gallery__item a.media-gallery__item-thumbnail img').setAttribute('src', img_url);
+        media_gallery.appendChild(t);
+        num++;
     });
 
     return media_gallery;
